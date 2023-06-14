23b5aac171de76c46909324d18a66d4594dfb2b9
StanMungai <stanfordkangethe@outlook.com>
2023-06-14 15:21:38 +0300

    Done with the first JSX lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,17 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const styles = {
+    color: 'firebrick'
+  }
+
+  return (
+    <div id="home">
+      <h1 style={styles}>
+        {name} is a Web Developer from {city}
+      </h1>
+    </div>
+  )
 }
 
 export default Home;
